e38a4c80d7769351d724979223ddb78f7a270e05
Ana Tiru <tiru.ana21@gmail.com>
2019-02-17 20:50:19 +0200

    add filter parameter

--- R/tempo_geo.R
+++ R/tempo_geo.R
@@ -11,18 +11,24 @@
 #' @param area - a string to set the type of area (counties, regions, macroregions)
 #' used to group Geospatial Data
 #' 
+#' @param filter - Character vector containing values from factor variables.
+#' 
+#' @param title - a string to set the map title
+#' 
 #' @return Returns a ggplot object. 
 #' 
 #' @examples 
-#' tempo_plot(matrix=AGR111A, year="2000", area="counties")
+#' tempo_geo(matrix = AGR111A, year = "2000", area = "counties", 
+#'           filter = c("Struguri de masa","Sector privat"),
+#'           title = "Hectare struguri de masa pe judete")
 #' 
 #' @import dplyr
 #' @import ggplot2
 #' @export
 
-tempo_geo <- function(matrix, year, area) {
+tempo_geo <- function(matrix, year, area, filter = NULL, title = NULL) {
   
-  if (nargs() != 3) {
+  if (nargs() < 3) {
     print("Wrong number of arguments!")
     return (NULL)
   }
@@ -44,7 +50,7 @@ tempo_geo <- function(matrix, year, area) {
     return (NULL)
   }
   
-  matrix <- subset_default(matrix)
+  matrix <- subset_filter(matrix, filter)
   
   # year parameter
   column_names <- names(matrix)
@@ -68,8 +74,6 @@ tempo_geo <- function(matrix, year, area) {
   pos_column_val <- grep("(valoare)", tolower(column_names))
   pos_column_loc <- which(names(matrix)=="Localitati")
   
-  load("data/df_coordinates.rda")
-  
   if(area == "counties") {
     if(length(pos_column_jud)==0) {
       cat("No data available for counties!\n")
@@ -80,9 +84,9 @@ tempo_geo <- function(matrix, year, area) {
     }
     rows_to_remove <- grep("(regiunea|macroregiunea|total)", tolower(matrix[,pos_column_jud]))
     matrix <- matrix[-rows_to_remove,]
+    title_def <- paste(unname(as.matrix(matrix[1,-c(pos_column_jud, pos_column_val, pos_column_year)])), collapse = "")
     label <- aggregate(cbind(long, lat) ~ mnemonic, data=df_coordinates, FUN=function(x)mean(range(x)))
     matrix[,pos_column_jud] <- trimws(as.character(matrix[,pos_column_jud]))
-    title <- paste(unname(as.matrix(matrix[1,-c(pos_column_jud, pos_column_val, pos_column_year)])), collapse = "")
     df_coordinates$county <- trimws(df_coordinates$county)
     df_coordinates <- left_join(df_coordinates, matrix[,c(pos_column_jud, pos_column_val)], by = c("county"=column_names[pos_column_jud]))
   }
@@ -98,7 +102,7 @@ tempo_geo <- function(matrix, year, area) {
     label <- aggregate(cbind(long, lat) ~ region, data=df_coordinates, FUN=function(x)mean(range(x)))
     matrix[,pos_column_reg] <- trimws(as.character(matrix[,pos_column_reg]))
     matrix[,pos_column_macroreg] <- gsub(" - ","-", matrix[,pos_column_macroreg])
-    title <- paste(unname(as.matrix(matrix[1,-c(pos_column_reg, pos_column_val, pos_column_year)])), collapse = "")
+    title_def <- paste(unname(as.matrix(matrix[1,-c(pos_column_reg, pos_column_val, pos_column_year)])), collapse = "")
     df_coordinates$region <- trimws(df_coordinates$region)
     df_coordinates <- left_join(df_coordinates, matrix[,c(pos_column_reg, pos_column_val)], by = c("region"=column_names[pos_column_reg]))
   }
@@ -112,40 +116,80 @@ tempo_geo <- function(matrix, year, area) {
     matrix <- matrix[set_reg,]
     label <- aggregate(cbind(long, lat) ~ macroregion, data=df_coordinates, FUN=mean)
     matrix[,pos_column_macroreg] <- trimws(as.character(matrix[,pos_column_macroreg]))
-    
-    title <- paste(unname(as.matrix(matrix[1,-c(pos_column_reg, pos_column_val, pos_column_year)])), collapse = "")
+    title_def <- paste(unname(as.matrix(matrix[1,-c(pos_column_macroreg, pos_column_val, pos_column_year)])), collapse = "")
     df_coordinates$macroregion <- trimws(df_coordinates$macroregion)
     df_coordinates <- left_join(df_coordinates, matrix[,c(pos_column_macroreg, pos_column_val)], by = c("macroregion"=column_names[pos_column_macroreg]))
   }
   
-  title <- paste(title, "Anul", year)
-  options(scipen=999)
-  plot <- ggplot(df_coordinates) +  
-    theme_bw() + 
-    geom_polygon(aes(long, lat, group=mnemonic, fill=df_coordinates[,ncol(df_coordinates)]), colour = "grey") +
-    geom_text(data=label, aes(long, lat, label=label[,1]), size=3, vjust=0) +
-    scale_fill_gradient(low='white', high='red', name = column_names[pos_column_val])+
-    ggtitle(title) +
-    coord_fixed(1)
-  
-  return(plot)
+  if (is.null(title)) {
+    title <- paste(title_def, "Anul", year)
+  }
+  legend_name <- column_names[pos_column_val]
+  plot <- plot_map(df_coordinates, title, legend_name, label, tmp)
+  return (plot)
 }
 
-subset_default <- function(matrix) {
+subset_filter <- function(matrix, filter) {
   matrix <- tempo_clean(matrix)
   column_names <- names(matrix)
   pos <- grep(("regiuni|judete|macroregiuni|ani$|localitati|valoare"), tolower(column_names))
   
-  n = seq(ncol(matrix)) 
-  n = n[-pos]
+  n <- seq(ncol(matrix)) 
+  n <- n[-pos]
   
-  for(i in n) {
-    lvs <- levels(matrix[,i])
-    if(length(lvs)>1){
-      lv <- lvs[2]
-      matrix <- subset(matrix, matrix[,i] == lv)
+  if (is.null(filter)) {
+    for(i in n) {
+      lvs <- levels(matrix[,i])
+      if(length(lvs)>1){
+        lv <- lvs[2]
+        matrix <- subset(matrix, matrix[,i] == lv)
+      }
+    }
+  } else {
+    
+    if(length(filter) != length(n)){
+      cat("Not enough filters! Add ", length(n), " filters!\n")
+      return (NULL)
+    }
+    
+    j <- 1;
+    for(i in n) {
+      lvs <- trimws(levels(matrix[,i]))
+      lv <- trimws(filter[j])
+      
+      if (!(lv %in% lvs)) {
+        cat("Incorrect filter! Choose filter from: ", lvs, "\n")
+        return (NULL)
+      }
+      
+      if (length(lvs) > 1) {
+        matrix <- subset(matrix, trimws(matrix[,i]) == lv)
+      }
+      j <- j+1
     }
   }
+
   return(matrix)
 }
 
+plot_map <- function(df_coordinates, title, legend_name, label, tmp){
+  options(scipen=999)
+  
+  plot <- ggplot(df_coordinates) +  
+    theme_bw() + 
+    geom_polygon(aes(long, lat, group=mnemonic, fill=df_coordinates[,ncol(df_coordinates)]), colour = "black") +
+    geom_text(data=label, aes(long, lat, label=label[,1]), size=3, vjust=0) +
+    scale_fill_gradient(low='white', high='red', na.value = "gray", name = legend_name)+
+    ggtitle(title) +
+    coord_fixed(1) 
+  
+  if (anyNA(df_coordinates)) {
+    plot <- plot +
+      geom_polygon(aes(c(40), c(40), colour="")) +
+      guides(colour=guide_legend("No data", override.aes=list(colour="gray1", fill="gray")))
+  }
+  plot_name <- paste0(tmp, "_map.pdf")
+  ggsave(plot_name, width = 20, height = 20, units = "cm")
+  
+  return(plot)
+}
